b40a9608aa6a94b951ccf1328f9ac342d90951af
Vbonnier <103245255+Vbonnier@users.noreply.github.com>
2022-07-05 16:52:12 +0200

    ajout du header

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+
+DEFINE("BDD", "tchat");
+DEFINE("USER", "root");
+DEFINE("HOST", "localhost");
+DEFINE("PASSW", "");
+DEFINE("CHARSET", "utf8mb4");
+
+// On teste la connexion à la BDD
+try {
+    $connexion= new PDO ("mysql:host=".HOST.";dbname=".BDD.";charset=utf8", USER, PASSW, array(PDO::ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION));
+}
+catch (PDOException $exception) {
+    die('Erreur fonction connexion : ' . $exception->getMessage());
+}
+
+
+?>
