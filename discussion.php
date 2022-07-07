7cae579e359f90fd05b848465d4acbd2a7e9a232
Vbonnier <103245255+Vbonnier@users.noreply.github.com>
2022-07-07 09:18:38 +0200

    ok

--- discussion.php
+++ discussion.php
@@ -43,6 +43,9 @@
         
         </div>
 </div>
+
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
