69cee5253f2f83a0e63b95d5ff5e2fec42a3ae04
Zoubir <hhhzoubir4gmail.com>
2022-07-07 09:06:59 +0200

    modif discussion bouton"send"

--- discussion.php
+++ discussion.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Discussion</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
     crossorigin="anonymous">
@@ -13,42 +14,36 @@
 </head>
 <body>
 
-<!-- <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src=""
-                                class="icon"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <button class="btn btn-dark " type="submit"><a href="index1.html">Mon Avatar</a></button>
-                        &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-                        <input type="text" class="searching" textearea>
-                        <button class="btn btn-dark" type="submit"><a href="index2.html"></a>ok</button>
-                        &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-                        <button class="btn btn-dark " type="submit"><a href="index3.html">Tchat privé</a></button>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 " id="wrapper">
+            <div id="menu">
+                <p class="welcome">Welcome, <b></b></p>
+                <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+                <div style="clear:both"></div>
+            </div>
+     
+            <div id="chatbox"></div>
+     
+    </div>        
+            <form name="message" action="">
+                <input name="usermsg" type="text" id="usermsg" size="63" />
+                <button class=" btn btn-secondary">SEND</button>
+                
+            </form>
 
-                </div>
-        </div>
-    </nav> -->
-
-    <div id="wrapper">
-  <div id="menu">
-        <p class="welcome">Welcome, <b></b></p>
-        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-        <div style="clear:both"></div>
     </div>
-     
-    <div id="chatbox"></div>
-     
-    <form name="message" action="">
-        <input name="usermsg" type="text" id="usermsg" size="63" />
-        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-    </form>
+
+        <div class="row">
+        <div class="col-md-3 bg-light">
+                <input type="submit" >
+        </div>
+        
+        </div>
 </div>
+
+
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 <script type="text/javascript">
 // jQuery Document
